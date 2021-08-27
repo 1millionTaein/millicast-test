c67f25cbdd5c7fce00ac75f24f7403ca3aa54e8d
Taein Kim <taein@1milliondance.com>
2021-08-27 15:25:01 +0900

    feat : 라우터

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
-import LiveCast from "./components/LiveCast";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
+import LiveCast from "./components/LiveCast";
 import Subscribe from "./components/Subscribe";
+import Main from "./components/Main";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Header />
         <Switch>
-          <Route exact path="/" component={LiveCast} />
+          <Route exact path="/" component={Main} />
+          <Route path="/master" component={LiveCast} />
           <Route path="/subscribe" component={Subscribe} />
         </Switch>
       </div>
